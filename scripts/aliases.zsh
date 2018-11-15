8994dd480d9dca8f4c4ba313e58030ea273dfb32
Takashi Yoshida <takashi.yoshida@asia.thalesgroup.com>
2018-11-15 10:33:56 +0800

    Add alias for svn info

--- scripts/aliases.zsh
+++ scripts/aliases.zsh
@@ -5,6 +5,7 @@ alias tailf="tail -f"
 alias history="history -i"
 
 alias svndiff="svn diff --diff-cmd /usr/bin/diff -x -w"
+alias svninfo="svn info --show-item=url"
 alias cdpr="cd ${HOME}/Projects"
 
 alias lh="ls -lh"
