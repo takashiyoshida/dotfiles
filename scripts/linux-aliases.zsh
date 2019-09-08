22205264af03cc90e18cf28ccb1d2b30e3833c11
Takashi Yoshida <takashi@takashiyoshida.org>
2019-09-08 11:12:43 +0800

    Add a new alias for apt list --installed

--- scripts/linux-aliases.zsh
+++ scripts/linux-aliases.zsh
@@ -1,3 +1,4 @@
 alias ls="ls -CF --color=auto"
 alias vless="/usr/share/vim/vim80/macros/less.sh"
 alias ghtoken="xclip -sel clip < ${HOME}/.github-tokens.txt"
+alias apti="apt list --installed 2> /dev/null"
