5430bf1f9197d396e9c5ce42224399cbef8ce383
Takashi Yoshida <takashi@takashiyoshida.org>
2019-10-21 09:38:42 +0800

    Update path to vim-related scripts

--- scripts/linux-aliases.zsh
+++ scripts/linux-aliases.zsh
@@ -1,4 +1,4 @@
 alias ls="ls -CF --color=auto"
-alias vless="/usr/share/vim/vim80/macros/less.sh"
+alias vless="/usr/share/vim/vim81/macros/less.sh"
 alias ghtoken="xclip -sel clip < ${HOME}/.github-tokens.txt"
 alias apti="apt list --installed 2> /dev/null"
