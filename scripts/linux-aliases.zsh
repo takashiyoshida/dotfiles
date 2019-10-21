861981086f0c90615f1fcb48af651b076303ed19
Takashi Yoshida <takashi@takashiyoshida.org>
2019-10-21 09:41:20 +0800

    Use xdg-open to open files from Terminal

--- scripts/linux-aliases.zsh
+++ scripts/linux-aliases.zsh
@@ -2,3 +2,5 @@ alias ls="ls -CF --color=auto"
 alias vless="/usr/share/vim/vim81/macros/less.sh"
 alias ghtoken="xclip -sel clip < ${HOME}/.github-tokens.txt"
 alias apti="apt list --installed 2> /dev/null"
+
+alias open="xdg-open"
