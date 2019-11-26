af777e2c97ba7624d333652d79d40e17f7b64ba3
Takashi Yoshida <takashi@takashiyoshida.org>
2019-11-26 10:40:29 +0800

    Build OS-specific aliases first

--- scripts/aliases.zsh
+++ scripts/aliases.zsh
@@ -1,3 +1,15 @@
+# Set the OS specific aliases first, then string together other aliases the build on them
+# OS X specific aliases
+if [[ "${OSTYPE}" =~ darwin* ]]; then
+    source "${DOTFILES}/mac-aliases.zsh"
+else
+    source "${DOTFILES}/linux-aliases.zsh"
+fi
+
+# Common set of aliases
+alias lh="ls -lh"
+alias lll="ll | less"
+
 # Set TERM to xterm-256color to avoid incorrect color from being used
 # in tmux session
 alias emacs="TERM=xterm-256color emacs -nw"
@@ -7,25 +19,14 @@ alias history="history -i"
 alias svndiff="svn diff --diff-cmd /usr/bin/diff -x -w"
 alias svnldiff="svndiff -r COMMITTED" # Show the last commit
 alias svninfo="svn info --show-item=url"
-alias cdpr="cd ${HOME}/Projects"
-
-alias lh="ls -lh"
-
-# Create a new standup notes with today's date
-alias standup="emacs `date +%F-standup.md`"
 # Ignore unversioned files from svn status
 alias svnst="svn status -q"
+
+alias cdpr="cd ${HOME}/Projects"
 alias env="env | sort"
 
 # Remove emacs backup files
 alias rmeb="find . -name \"*~\" -delete"
-
-# OS X specific aliases
-if [[ "${OSTYPE}" =~ darwin* ]]; then
-    source "${DOTFILES}/mac-aliases.zsh"
-else
-    source "${DOTFILES}/linux-aliases.zsh"
-fi
 
 # Useful functions to convert a hexadecimal number to a decimal number and vice versa
 # http://www.cyberciti.biz/faq/linux-unix-convert-hex-to-decimal-number/
