b3ebca365958794f3788e23e42761805bdbbec52
Takashi Yoshida <takashi@takashiyoshida.org>
2020-02-18 16:10:39 +0800

    Allow db-prepper to export database files for multiple environment

--- bin/db-prepper.py
+++ bin/db-prepper.py
@@ -7,6 +7,10 @@ import os
 import os.path
 import shutil
 
+# A list of valid environment
+ENVIRONMENT = ['ATS', 'BGK', 'BNK', 'CMS', 'CNT', 'CQY', 'DBG', 'ECS', 'EMS', 'FRP',
+               'HBF', 'HGN', 'KVN', 'LTI', 'NED', 'OTP', 'PGC', 'PGL', 'PTP', 'SER', 'SKG', 'WLH']
+
 
 def init_logging():
     '''
@@ -16,11 +20,11 @@ def init_logging():
     logger.setLevel(logging.DEBUG)
 
     c = logging.StreamHandler()
-    c_formatter = logging.Formatter(fmt='%(levelname)s %(message)s')
+    c_formatter = logging.Formatter(fmt='%(message)s')
     c.setFormatter(c_formatter)
     c.setLevel(logging.INFO)
 
-    f = logging.FileHandler('manticore.log')
+    f = logging.FileHandler('db-prepper.log')
     f_formatter = logging.Formatter(
         fmt='%(asctime)s %(levelname)s %(message)s')
     f.setFormatter(f_formatter)
@@ -31,20 +35,33 @@ def init_logging():
     return logger
 
 
+def is_environment_valid(environment):
+    '''
+    '''
+    try:
+        ENVIRONMENT.index(environment.upper())
+        return True
+    except ValueError:
+        logging.error('%s is not a valid environment name', environment)
+        return False
+
+
 def directory_exists(directory):
     '''
+    Returns True when the given directory exists
     '''
-    logging.info('Checking if %s exists...', directory)
+    logging.info('Checking if %s exists ...', directory)
     if os.path.isdir(directory):
         logging.info('OK')
         return True
 
-    logging.error('%s does not exist', directory)
+    logging.info('The directory %s does not exist', directory)
     return False
 
 
 def create_directory(directory):
     '''
+    Creates a new directory if it does not exist
     '''
     if not directory_exists(directory):
         try:
@@ -60,22 +77,55 @@ def create_directory(directory):
 
 def file_exists(infile):
     '''
+    Returns True when the given file exists
     '''
-    logging.info('Checking if %s exists...', infile)
+    logging.info('Checking if %s exists ...', infile)
     if os.path.isfile(infile):
         logging.info('OK')
         return True
 
-    logging.error('%s does not exist', infile)
+    logging.info('The file %s does not exist', infile)
     return False
 
 
 def prerequisites_exists(database_path, environment):
     '''
+    Each database package needs the following files and directories:
+
+    - Archives (directory)
+      CMS and ECS have additional pre-requisites
+
+      For CMS:
+      DB_CMS/POWLIGHT.cfg
+      DB_CMS/POWMXD.cfg
+
+      For ECS:
+      DB_ECS/ECS.cfg
+
+    - dac_DB_xxx (directory)
+    - dac_DB_xxx/xxx/ScsDacCtrt.cfg (file)
+
+    - Database/xml_DB_xxx (directory)
+    - Database/classlist_xxx.txt (file)
     '''
+
+    logging.info('Checking pre-requisites for %s environment ...', environment)
+
     archives_dir = '%s/Archives/DB_%s' % (database_path, environment)
     if not directory_exists(archives_dir):
         return False
+
+    if environment == 'CMS':
+        powlight_cfg = '%s/POWLIGHT.cfg' % (archives_dir)
+        if not file_exists(powlight_cfg):
+            return False
+        powmxd_cfg = '%s/POWMXD.cfg' % (archives_dir)
+        if not file_exists(powmxd_cfg):
+            return False
+    elif environment == 'ECS':
+        ecs_cfg = '%s/ECS.cfg' % (archives_dir)
+        if not file_exists(ecs_cfg):
+            return False
 
     dac_dir = '%s/dac_DB_%s' % (database_path, environment)
     if not directory_exists(dac_dir):
@@ -95,12 +145,16 @@ def prerequisites_exists(database_path, environment):
     if not file_exists(classlist):
         return False
 
+    logging.info('All pre-requisites for %s environment ... OK', environment)
     return True
 
 
 def create_export_directory(directory):
     '''
     '''
+    logging.info(
+        'Creating export directory for %s environment ...', directory)
+
     if not create_directory(directory):
         return False
 
@@ -128,7 +182,74 @@ def create_export_directory(directory):
     if not create_directory(database_dir):
         return False
 
+    logging.info(
+        'Created the export directory %s successfully ...', directory)
     return True
+
+
+def export_archives(database_path, environ):
+    '''
+    '''
+    source_path = os.path.join(database_path, 'Archives')
+    export_path = os.path.join(environ, 'Archives')
+
+    if environ == 'CMS':
+        source_item = os.path.join(source_path, 'POWLIGHT.cfg')
+        export_item = os.path.join(
+            export_path, 'CMST_POWLIGHT', 'CMST_POWLIGHT.cfg')
+        logging.info('Exporting %s to %s ...', source_item, export_item)
+        shutil.copy2(source_item, export_item)
+
+        source_item = os.path.join(source_path, 'POWMXD.cfg')
+        export_item = os.path.join(
+            export_path, 'CMST_POWMXD', 'CMST_POWMXD.cfg')
+        logging.info('Exporting %s to %s ...', source_item, export_item)
+        shutil.copy2(source_item, export_item)
+    elif environ == 'ECS':
+        source_item = os.path.join(source_path, 'ECS.cfg')
+        export_item = os.path.join(export_path, 'ECST_ECS', 'ECST_ECS.cfg')
+        logging.info('Exporting %s to %s ...', source_item, export_item)
+        shutil.copy2(source_item, export_item)
+
+
+def export_dac_files(database_path, environ):
+    '''
+    '''
+    source_path = os.path.join(database_path, 'dac_DB_%s' % (environ))
+    export_path = os.path.join(environ, 'dac')
+
+    for item in os.listdir(source_path):
+        if item.endswith('.dat'):
+            source_item = os.path.join(source_path, item)
+            logging.info('Exporting %s to %s ...', source_item, export_path)
+            shutil.copy2(source_item, export_path)
+
+    source_item = os.path.join(source_path, environ, 'ScsDacCtrt.cfg')
+    logging.info('Exporting %s to %s ...', source_item, export_path)
+    shutil.copy2(source_item, export_path)
+
+
+def export_database_files(database_path, environ):
+    '''
+    '''
+    source_path = os.path.join(database_path, 'Database')
+    export_path = os.path.join(environ, 'Database')
+
+    source_item = os.path.join(source_path, 'xml_DB_%s' % (environ))
+    export_item = os.path.join(export_path, 'xml')
+
+    if directory_exists(export_item):
+        logging.info('Removing the existing %s directory ...', export_item)
+        shutil.rmtree(export_item)
+
+    logging.info('Exporting %s to %s ...', source_item, export_item)
+    shutil.copytree(source_item, export_item)
+
+    source_item = os.path.join(source_path, 'classlist_%s.txt' % (environ))
+    export_item = os.path.join(export_path, 'classlist.txt')
+
+    logging.info('Exporting %s to %s ...', source_item, export_item)
+    shutil.copy2(source_item, export_item)
 
 
 def main():
@@ -140,128 +261,40 @@ def main():
     parser = argparse.ArgumentParser(prog='db-prepper')
     parser.add_argument('--database-path', '-d', required=True,
                         dest='database_path', help='path to database directory')
-    parser.add_argument('--environment', '-e', required=True,
+    parser.add_argument('--environment', '-e', nargs='+', required=True,
                         dest='environment', help='name of ISCS environment')
 
     args = parser.parse_args()
-    logging.info('args: %s', args)
 
-    '''
-    Check if all required files and directories exist.
-    Each database package should have the following:
-    - Archives/DB_xxx
-    - dac_DB_xxx
-    - dac_DB_xxx/xxx/ScsDacCtrt.cfg
-    - Database/xml_DB_xxx
-    - Database/classlist_xxx.txt
+    logging.debug('database_path: %s', args.database_path)
 
-    We should be able to find these under the database_path.
-    '''
-    result = prerequisites_exists(args.database_path, args.environment)
-    if not result:
-        exit(1)
+    for environ in args.environment:
+        environ = environ.upper()
+        logging.debug('environment: %s', environ)
 
-    '''
-    Create a template directory for the environment
-    Each environment database package should have the following directory:
-    - xxx/Archives
-    - xxx/dac
-    - xxx/Database
-    '''
-    result = create_export_directory(args.environment)
-    if not result:
-        exit(1)
+        if not is_environment_valid(environ):
+            logging.warn(
+                '%s is not a valid ISCS environment name. Skipping ...', environ)
+            continue
 
-    '''
-    Copy the following files and directories:
-    - Archives/DB_xxx (this will be handled later)
-    - dac_DB_xxx/*.dat to dac/*.dat
-    - dac_DB_xxx/xxx/ScsDacCtrt.cfg to dac/ScsDacCtrt.cfg
-    - Database/xml_DB_xxx to Database/xml
-    - Database/classlist_xxx.txt to Database/classlist.txt
-    '''
+        if not prerequisites_exists(args.database_path, environ):
+            logging.warn(
+                '%s does not contain pre-requisites for %s environment; Skipping ...',
+                args.database_path, environ)
+            continue
 
-    '''
-    CMS
-    - Archives/CMST_POWLIGHT/CMST_POWLIGHT.cfg
-    - Archives/CMST_POWMXD/CMST_POWMXD.cfg
+        if not create_export_directory(environ):
+            logging.warn(
+                'Failed to create an export directory for %s environment. Skipping ...',
+                environ)
+            continue
 
-    ECS
-    - Archives/ECST_ECS/ECST_ECS.cfg
+        export_archives(args.database_path, environ)
+        export_dac_files(args.database_path, environ)
+        export_database_files(args.database_path, environ)
 
-    ATS
-    - Archives is empty
-
-    DBS
-    - There are no DB package for DBS
-
-    EMS
-    - Archives is empty
-
-    NED
-    - Archives is empty
-    '''
-
-    source_path = '%s/Archives' % (args.database_path)
-    dest_path = '%s/Archives' % (args.environment)
-
-    # For now, assume that only CMS and ECS have the Archives with meaningful data
-    if args.environment == 'CMS':
-        logging.info('Copying Archives configuration files for CMS ...')
-        '''
-        Archives/POWLIGHT.cfg -> Archives/CMST_POWLIGHT/CMST_POWLIGHT.cfg
-        Archives/POWMXD.cfg -> Archives/CMST_POWMXD/CMST_POWMXD.cfg
-        '''
-        source_item = '%s/POWLIGHT.cfg' % (source_path)
-        dest_item = '%s/CMST_POWLIGHT/CMST_POWLIGHT.cfg' % (dest_path)
-        shutil.copy2(source_item, dest_item)
-
-        source_item = '%s/POWMXD.cfg' % (source_path)
-        dest_item = '%s/CMST_POWMXD/CMST_POWMXD.cfg' % (dest_path)
-        shutil.copy2(source_item, dest_item)
-
-    elif args.environment == 'ECS':
-        logging.info('Copying Archives configuration files for ECS ...')
-        '''
-        Archives/ECS.cfg -> Archives/ECST_ECS/ECST_ECS.cfg
-        '''
-        source_item = '%s/ECS.cfg' % (source_path)
-        dest_item = '%s/ECST_ECS/ECST_ECS.cfg' % (dest_path)
-        shutil.copy2(source_item, dest_item)
-
-    source_path = '%s/dac_DB_%s/' % (args.database_path, args.environment)
-    dest_path = '%s/dac' % (args.environment)
-
-    logging.info('Copying *.dat files from %s to %s ...',
-                 source_path, dest_path)
-    for item in os.listdir(source_path):
-        if item.endswith('.dat'):
-            source_file = '%s/%s' % (source_path, item)
-            shutil.copy2(source_file, dest_path)
-
-    source_item = '%s/%s/ScsDacCtrt.cfg' % (source_path, args.environment)
-    logging.info('Copying ScsDacCtrt.cfg from %s to %s ...',
-                 source_path, dest_path)
-    shutil.copy2(source_item, dest_path)
-
-    source_path = '%s/Database' % (args.database_path)
-    dest_path = '%s/Database' % (args.environment)
-
-    # Copy database xml_DB_xxx
-
-    source_item = '%s/xml_DB_%s' % (source_path, args.environment)
-    dest_item = '%s/xml' % (dest_path)
-
-    logging.info('Copying %s to %s ...', source_item, dest_item)
-    shutil.copytree(source_item, dest_item)
-
-    # Copy database classlist_xxx.txt
-
-    source_item = '%s/classlist_%s.txt' % (source_path, args.environment)
-    dest_item = '%s/classlist.txt' % (dest_path)
-    logging.info('Copying classlist_%s.txt to %s ...',
-                 args.environment, dest_item)
-    shutil.copy2(source_item, dest_item)
+        logging.info(
+            'Prepping database files for %s environment complete ...', environ)
 
 
 if __name__ == '__main__':
