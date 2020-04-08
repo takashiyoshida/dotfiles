c3666e8e4eacdf88da6b397a8bb07720f12d1aa3
Takashi Yoshida <takashi@takashiyoshida.org>
2020-04-08 15:13:13 +0800

    Create a generic function for C755A and C755B projects

--- scripts/aliases.zsh
+++ scripts/aliases.zsh
@@ -72,7 +72,7 @@ function dirsize
     du -h -d 1 ${MY_DIR} | sort -h -r
 }
 
-function mk-c755a-workspace
+function mkdev
 {
     workspace=${PWD}
     if [[ $# != 0 ]]; then
@@ -134,71 +134,13 @@ function mk-c755a-workspace
     tmux attach -t ${session_name}
 }
 
-function mk-c755b-workspace
+alias mkdev.c755a="mkdev ${HOME}/Projects/c755a-fix-bugs"
+alias mkdev.c755b="mkdev ${HOME}/Projects/c755b-dev"
+
+
+function mkrturep
 {
-    workspace=${PWD}
-    if [[ $# != 0 ]]; then
-        workspace=$1
-        if [ ! -d ${workspace} ]; then
-            echo "${workspace} does not exist"
-            return
-        fi
-    fi
-
-    session_name=$(basename ${workspace})
-    tmux has-session -t ${session_name} 2> /dev/null
-    if [ $? != 0 ]; then
-        tmux detach
-
-        cd ${workspace}
-
-        tmux new-session -s ${session_name} -n build -d
-        tmux split-window -h -t build
-        tmux select-pane -t :.1 # Move the focus to pane 1 of build window
-
-        # Administrating test VM (i.e. install new packages, etc)
-        tmux new-window -n admin
-
-        # Split ATS window in half (one for running ATS and another for logs)
-        tmux new-window -n ATS
-        tmux split-window -h -t ATS
-        tmux select-pane -t :.1 # Move the focus to pane 1 of the ATS window
-
-        # Split CMS window in half (one for running CMS and another for logs)
-        tmux new-window -n CMS
-        tmux split-window -h -t CMS
-        tmux select-pane -t :.1 # Move the focus to pane 1 of the CMS window
-
-        # Split ECS window in half (one for running ECS and another for logs)
-        tmux new-window -n ECS
-        tmux split-window -h -t ECS
-        tmux select-pane -t :.1 # Move the focus to pane 1 of the ECS window
-
-        # Split NED window in half (one for running NED and another for logs)
-        tmux new-window -n NED
-        tmux split-window -h -t NED
-        tmux select-pane -t :.1 # Move the focus to pane 1 of the NED window
-
-        # Split PGC window in half (one for running PGC and another for logs)
-        tmux new-window -n PGC
-        tmux split-window -h -t PGC
-        tmux select-pane -t :.1 # Move the focus to pane 1 of the PGC window
-
-        # Split SIM window in half (one for running SIM and another for logs)
-        tmux new-window -n SIM
-        tmux split-window -h -t SIM
-        tmux select-pane -t :.1 # Move the focus to pane 1 of the SIM window
-
-        # Go back to the build window and select the first pane
-        tmux select-window -t build
-        tmux select-pane -t :.1
-    fi
-    tmux attach -t ${session_name}
-}
-
-function mk-rturep-workspace
-{
-    workspace=${PWD}
+    workspace="${HOME}/Projects/rturep-dev"
     if [[ $# != 0 ]]; then
         workspace=$1
         if [ ! -d ${workspace} ]; then
