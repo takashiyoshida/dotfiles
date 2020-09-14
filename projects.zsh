0b816b652ab6cb0d4e50826cba2c63549297d622
Takashi Yoshida <takashi.yoshida@asia.thalesgroup.com>
2020-09-14 16:40:15 +0800

    Add environment variables for provisioning neltrs1a and nelscs1a

--- projects.zsh
+++ projects.zsh
@@ -2,3 +2,9 @@
 # i.e. JIRA username, password
 export JIRA_USER=""
 export JIRA_PASS=""
+
+NELARCHIVER_IP=$(grep "nelarchiver" /etc/hosts | awk '{ print $1 }')
+export NELARCHIVER_IP
+
+NELARCHIVER_PORT="80"
+export NELARCHIVER_PORT
