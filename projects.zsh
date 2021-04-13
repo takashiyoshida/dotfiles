cfae24eb6cac92e4041473432897ffdd9dfcd196
Takashi Yoshida <takashi@takashiyoshida.org>
2021-04-13 23:30:24 +0800

    Move secrets from projects.zsh to secrets.zsh

--- projects.zsh
+++ projects.zsh
@@ -1,8 +1,3 @@
-# Add project-related configurations or secrets here
-# i.e. JIRA username, password
-export JIRA_USER=""
-export JIRA_PASS=""
-
 NELARCHIVER_IP=$(grep "nelarchiver" /etc/hosts | awk '{ print $1 }')
 NELARCHIVER_PORT="80"
 
---
+++ secrets.zsh
@@ -0,0 +1,7 @@
+# Homebrew access token
+export HOMEBREW_GITHUB_API_TOKEN=""
+
+# Add project-related configurations or secrets here
+# i.e. JIRA username, password
+export JIRA_USER=""
+export JIRA_PASS=""
