c571f2ad002b05bea62ab9feaaae44ff1a8e8e2e
Takashi Yoshida <takashi@takashiyoshida.org>
2022-01-30 21:18:43 +0800

    Add iterm2 integration and powerlevel10k configurations

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,3 +1,10 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 if [[ "${OSTYPE}" =~ linux-gnu* ]]; then
     # Source .zprofile
     case $- in
@@ -83,4 +90,15 @@ source "${HOME}/.secrets.zsh"
 add-ssh-private-keys
 
 # Start emacs service (macOS)
-start-emacs-service
+if [[ "${OSTYPE}" =~ darwin* ]]; then
+    start-emacs-service
+fi
+
+if [ -e "${HOME}/.iterm2_shell_integration.zsh" ]; then
+    source "${HOME}/.iterm2_shell_integration.zsh"
+fi
+
+source ${HOME}/Projects/powerlevel10k/powerlevel10k.zsh-theme
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f "${HOME}/.p10k.zsh" ]] || source ${HOME}/.p10k.zsh
