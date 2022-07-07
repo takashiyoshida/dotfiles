850704d2325aeb06afc9c130d301250e2d03801a
Takashi Yoshida <takashi@takashiyoshida.org>
2022-07-07 10:23:50 +0800

    Use the correct network interface card

--- projects.zsh
+++ projects.zsh
@@ -4,8 +4,9 @@ NELARCHIVER_PORT="80"
 if [ -z "${NELARCHIVER_IP}" ]; then
     # When not working from office, I will run a Docker container as a nelarchiver
     # Depending on your OS, you will need to change the network interface below
-    NELARCHIVER_IP=$(ifconfig en0 | grep --color=never "inet " | awk '{ print $2 }')
+    NELARCHIVER_IP=$(ifconfig enp7s0 | grep --color=never "inet " | awk '{ print $2 }')
     NELARCHIVER_PORT="8080"
 fi
 export NELARCHIVER_IP
 export NELARCHIVER_PORT
+
