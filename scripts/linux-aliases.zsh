c0e05fa2df81f742a348f7579a9b207730266b40
Takashi Yoshida <takashi@takashiyoshida.org>
2022-07-21 14:58:33 +0800

    Update path to vim

--- scripts/linux-aliases.zsh
+++ scripts/linux-aliases.zsh
@@ -1,7 +1,7 @@
 alias ls="ls -CF --color=auto"
 alias apti="apt list --installed 2> /dev/null"
 
-alias vless="/usr/share/vim/vim81/macros/less.sh"
+alias vless="/usr/share/vim/vim82/macros/less.sh"
 
 # Copy the Github API token from a hidden file
 alias ghtoken="xclip -sel clip < ${HOME}/.github-tokens.txt"
