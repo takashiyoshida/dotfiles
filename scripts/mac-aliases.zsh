16430a42a15adb885f241be980a37a1341fbb1aa
Takashi Yoshida <takashi@takashiyoshida.org>
2022-12-31 19:12:33 +0800

    Fix merge mistake

--- scripts/mac-aliases.zsh
+++ scripts/mac-aliases.zsh
@@ -3,15 +3,8 @@ alias top="top -ocpu -R -F -s 2"
 alias hub=git
 alias vless="/opt/homebrew/share/vim/vim82/macros/less.sh"
 
-<<<<<<< HEAD
 alias brout="brew update && brew outdated"
 alias brupg="brew upgrade"
-=======
-    alias ls="ls -CFG"
-    alias top="top -ocpu -R -F -s 2"
-    alias hub=git
-    alias vless="/opt/homebrew/share/vim/vim90/macros/less.sh"
->>>>>>> 637f857c6fad49cfb9bc3703239ac568c90deccc
 
 function ql
 {
