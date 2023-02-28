0515d57e79a5991468c06b09e858e5fb09188dc4
Takashi Yoshida <takashi@takashiyoshida.org>
2023-02-28 21:07:32 +0800

    Move fun stuff out of zprofile.zsh

--- projects.zsh
+++ projects.zsh
@@ -1,3 +1,11 @@
+# Random fun stuff
+if [[ "${OSTYPE}" =~ "darwin"* ]]; then
+    # This is taken care of in zshenv.sh
+    # echo "HOMEBREW_PREFIX=${HOMEBREW_PREFIX}"
+    # Display an interesting login message
+    ${HOMEBREW_PREFIX}/bin/fortune | ${HOMEBREW_PREFIX}/bin/cowsay | ${HOMEBREW_PREFIX}/bin/lolcat
+fi
+
 # Work-related configurations
 
 # network interface to be used for connecting to NELARCHIVER
