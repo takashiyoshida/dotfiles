87eb8d83f033e77d674311166090fcf0aa483d7c
Takashi Yoshida <takashi@takashiyoshida.org>
2023-03-16 19:23:09 +0800

    Force color in lolcal

--- projects.zsh
+++ projects.zsh
@@ -3,7 +3,7 @@ if [[ "${OSTYPE}" =~ "darwin"* ]]; then
     # This is taken care of in zshenv.sh
     # echo "HOMEBREW_PREFIX=${HOMEBREW_PREFIX}"
     # Display an interesting login message
-    ${HOMEBREW_PREFIX}/bin/fortune | ${HOMEBREW_PREFIX}/bin/cowsay | ${HOMEBREW_PREFIX}/bin/lolcat
+    ${HOMEBREW_PREFIX}/bin/fortune | ${HOMEBREW_PREFIX}/bin/cowsay | ${HOMEBREW_PREFIX}/bin/lolcat --force 
 fi
 
 # Work-related configurations
