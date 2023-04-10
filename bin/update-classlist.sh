18464b3c046efcace42e50aefeb5551df7b08d20
Takashi Yoshida <takashi@takashiyoshida.org>
2023-04-10 16:11:51 +0800

    Update CMST_POWMXD.cfg and ECST_ECS.cfg files

--- bin/update-classlist.sh
+++ bin/update-classlist.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
 NELDATA="../../../../src/nel/pkg/neldata"
+ECSDATA="../../../../src/mcs/ecs/dat"
+MXDDATA="../../../../src/mcs/pow/mxd/dat"
 
-cp -v classlist_*.txt "${NELDATA}/eqplists"
-cp -v dbmuserconst_class*.cfg "${NELDATA}/dbuserdata"
+(
+    cd Database
+    cp -v classlist_*.txt "${NELDATA}/eqplists"
+    cp -v dbmuserconst_class*.cfg "${NELDATA}/dbuserdata"
+)
+
+(
+    # C755B-1396: When ECST_ECS.cfg file is updated, we need to update
+    # ECST_ECS.cfg file at mcs/ecs/dat.
+    # This is because NELdbs environment uses this file for "something".
+    cd Archives
+
+    cp -v CMST_POWMXD.cfg "${MXDDATA}/CMST_POWMXD.cfg"
+    cp -v ECST_ECS.cfg "${ECSDATA}/ECST_ECS.cfg"
+)
